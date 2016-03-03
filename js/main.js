1fec2120880a0b6ba404400f097bb690f67d1698
sanchja <jasonsanc76@gmail.com>
2016-03-03 17:01:09 -0500

    New plots
    jquery added for functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,12 @@
 $(function() {
-    $(".region1").on('click', function(){
-		
-        $(".gregory_byrnes").addClass('animated fadeIn').css("visibility", "visible");
-
-	})
+//    $(".region1").on('click', function(){
+//        $(".gregory_byrnes").fadeToggle();
+//        $(".James_A_Gill").fadeToggle();
+        
+        
+       $("#card").flip({
+  trigger: 'manual'
+});
 	
 	
 	
