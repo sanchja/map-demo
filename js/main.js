9ea8a1a26fc8db5f46bd8b3000881da53be6c7b6
sanchja <jasonsanc76@gmail.com>
2016-03-07 03:30:13 -0500

    zoom and fade

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 $(function() {
     
-    $(".region1").on('click', function(){
+    //$(".region1").on('click', function(){
         //alert("zoom in on Region");
         
 //        $(".gregory_byrnes").fadeToggle();
@@ -26,5 +26,41 @@ $(function() {
         
 /*************************************/        
     
+        
+//    $(".gregory_byrnes").click(function() {
+//        $(this).animate({
+//            rotateY: "+=" + (Math.PI) / 2
+//        }, 500, function() {
+//            $(this).toggleClass('flipped'); // toggle class while the flip is in the middle
+//        }).animate({
+//            rotateY: "+=" + (Math.PI) / 2
+//        }, 500);
+//    });
+    
+    document.querySelector( '.region1' ).addEventListener( 'click', function( event ) {
+            event.preventDefault();
+            $('.usa_map').toggleClass('animated fadeOut');
+                $(".gregory_byrnes").fadeToggle();
+                $(".James_A_Gill").fadeToggle();
+            zoom.to({ element: event.target });
+        } );
+        
+         document.querySelector( '.zoom_samples' ).addEventListener( 'click', function( event ) {
+            event.preventDefault();
+            $('.usa_map').toggleClass('animated fadeOut');
+           // $(".gregory_byrnes").fadeToggle();
+   // $(".James_A_Gill").fadeToggle();
+            zoom.to({ element: event.target });
+        } );
+        
+        $('.gregory_byrnes').on('click',function(){
+            $(this).addClass('animated tada');
+        })
+        
+        $('.James_A_Gill').on('click',function(){
+            $(this).addClass('animated tada');
+        })
 	
 });
+    
+    
